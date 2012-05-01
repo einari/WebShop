a2e9813e7d98550dc8993cb5f405bbf07d065400
Einar Ingebrigtsen <einar@dolittle.com>
2012-05-01 21:18:57 +0200

    Cleanup

--- Source/WebShop.Domain/Carts/CartLine.cs
+++ Source/WebShop.Domain/Carts/CartLine.cs
@@ -6,8 +6,13 @@ namespace WebShop.Domain.Carts
 	{
 		public ProductId ProductId { get; set; }
 		public int Quantity { get; set; }
-		public decimal NetPrice { get; set; }
-		public decimal GrossPrice { get; set; }
+		
+		public decimal ItemNetPrice { get; set; }
+		public decimal ItemGrossPrice { get; set; }
+		
+		public decimal TotalNetPrice { get; set; }
+		public decimal TotalGrossPrice { get; set; }
+		
 		public decimal VatRate { get; set;}
 	}
 }
